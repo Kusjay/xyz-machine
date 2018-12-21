a432ac8d8c766cb5da7ccbb0f3cd775abd136e6c
Kusjay <kelechiuma@gmail.com>
2018-12-21 10:55:15 +0100

    initial commit

---
+++ xyz-machine/xyz-machine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Kusjay on 12/21/18.
+//  Copyright © 2018 Kusjay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
